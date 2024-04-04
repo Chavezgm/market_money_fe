de6c053e1c357af079e4380adfa376062a665519
Martin Chavez <martinchavez1203@gmail.com>
2024-04-04 17:04:21 -0600

    add id to poro

--- app/poro/market_poro.rb
+++ app/poro/market_poro.rb
@@ -1,7 +1,8 @@
 class MarketPoro
-  attr_reader :name, :city, :state
+  attr_reader :name, :city, :state, :id
   def initialize(attributes)
     # require 'pry'; binding.pry
+    @id = attributes[:id]
     @name = attributes[:attributes][:name]
     @city = attributes[:attributes][:city]
     @state = attributes[:attributes][:state]
